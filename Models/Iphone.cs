f0d531bd7015c37edcafbb83ecb7043c53491a97
Scarceli <diegoscarceli@hotmail.com>
2022-09-10 19:35:55 -0300

    Finalizado o projeto Smartphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,21 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+
+        public void AlterarNumero(string numeroAlterar){
+            Numero = numeroAlterar;
+            Console.WriteLine($"Seu número foi alterado para: {Numero}"); 
+        }
+        // Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Intalando o app {nomeApp} no Iphone");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("(11)11111-1111", "Modelo x10", "111111111111111", 1024);
+Iphone i1 = new Iphone("(22)22222-2222", "Modelo y20", "222222222222222", 2048);
+
+Console.WriteLine($"----- Smartphone Nokia -------"); 
+Console.WriteLine($"Número: {n1.Numero}");
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Outlook");
+n1.AlterarNumero("(11) 11111-2222");
+
+Console.WriteLine($"----- Smartphone Iphone -------"); 
+Console.WriteLine($"Número: {i1.Numero}");
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Outlook");
+i1.AlterarNumero("(22) 22222-3333");        
